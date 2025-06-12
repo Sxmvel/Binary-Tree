88f50e7743438a3b76e389d319fd5bb3e006ef13
Samuel Resende <samuelres444@gmail.com>
2025-06-12 19:55:25 -0300

    Implementa funcao que conta O numero de Nós não folha da ABB.

--- src/arvorebinariadebusca/ArvoreBinariaDeBusca.java
+++ src/arvorebinariadebusca/ArvoreBinariaDeBusca.java
@@ -10,18 +10,26 @@ public class ArvoreBinariaDeBusca {
     }
 
     private No inserirRec(No raiz, int chave) {
-
         if (raiz == null) {
             return new No(chave);
         }
-
         if (chave < raiz.chave) {
             raiz.esquerda = inserirRec(raiz.esquerda, chave);
         } else {
             raiz.direita = inserirRec(raiz.direita, chave);
         }
         return raiz;
+    }
 
+    // 1. Contar nós não-folhas
+    public int contarNaoFolhas() {
+        return NoNaoFolhas(raiz);
+    }
+    private int NoNaoFolhas(No no) {
+        if (no == null || (no.esquerda == null && no.direita == null)) {
+            return 0;
+        }
+        return 1 + NoNaoFolhas(no.esquerda) + NoNaoFolhas(no.direita);
     }
 
     public static void main(String[] args) {
